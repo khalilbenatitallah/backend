c28792bdeada2c24528c087063671dc5c65abb44
khalilbenatitallah <125036429+khalilbenatitallah@users.noreply.github.com>
2023-03-01 21:51:16 +0100

    message

---
+++ src/main/java/com/bezkoder/spring/login/models/ContactForm.java
@@ -0,0 +1,84 @@
+package com.bezkoder.spring.login.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class ContactForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String message;
+
+    // Constructeurs, getters et setters
+    public ContactForm() {
+    }
+
+    public ContactForm(String name, String email, String subject, String message) {
+        this.name = name;
+        this.email = email;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String toString()
+    {
+        return(this.email+","+this.message+","+this.name+","+this.subject+","+this.id);
+    }
+}
+
